3ae8e12fa76fa0049bf42469a47af8c0bfefc5a8
Marchesi Gabriele <marchesigabriele2819@gmail.com>
2021-06-01 21:30:51 +0200

    LongestSubstringWithoutRepeatingCharacters

---
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,28 @@
+# Longest Substring Without Repeating Characters.py
+
+
+def main():
+    s = "pwwkew"
+    print(fun(s))
+
+
+def fun(s):
+    old = []
+    max = 0
+    for i in range(len(s)):
+
+        print(old)
+        max = len(old) if len(old) > max else max
+
+        if old.__contains__(s[i]):
+            del old[0 : old.index(s[i]) + 1]
+
+        old.append(s[i])
+
+    max = len(old) if len(old) > max else max
+
+    return max
+
+
+if __name__ == "__main__":
+    main()
