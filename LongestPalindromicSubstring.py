1189084ad52fdd98030776044d96ac0e03ac3bea
Marchesi Gabriele <marchesigabriele2819@gmail.com>
2021-06-09 22:17:56 +0200

    LongestPalindromicSubstring

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -1,6 +1,8 @@
 # Given a string s, return the longest palindromic substring in s.
 
-s = "bddb"
+s = "ccc"
+st1 = 0
+st2 = 0
 
 
 def main():
@@ -14,7 +16,7 @@ def fun():
         c = s[i]
         new = check(i, i - 1, i + 1)
         c = ""
-        new1 = check1(i, i - 1, i + 1)
+        new1 = check1(i, i, i - 1, i + 1)
         max = new if len(new) > len(max) else max
         max = new1 if len(new1) > len(max) else max
     return max
@@ -33,19 +35,23 @@ def check(start, prima, dopo):
         return c
 
 
-def check1(start, prima, dopo):
+def check1(start1, start2, prima, dopo):
     global c
-    if prima >= 0:
-        if s[prima] == s[dopo]:
-            c = s[prima] + c + s[dopo]
-            check1(start, prima - 1, start + 1)
+    global st1
+    global st2
+    if prima >= 0 and start1 < len(s) and st1 == 0:
+        if s[prima] == s[start1]:
+            st2 = st2 + 1
+            c = s[prima] + c + s[start1]
+            check1(start1 + 1, start2, prima - 1, dopo)
             return c
         else:
             return c
-    elif dopo < len(s):
-        if s[dopo] == s[prima]:
-            c = s[prima] + c + s[dopo]
-            check1(start, start - 1, dopo + 1)
+    elif dopo < len(s) and start2 >= 0 and st2 == 0:
+        if s[dopo] == s[start2]:
+            st1 = st1 + 1
+            c = s[start2] + c + s[dopo]
+            check1(start1, start2 - 1, prima, dopo + 1)
             return c
         else:
             return c
