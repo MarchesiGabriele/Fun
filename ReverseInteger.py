9d5535d3bc0049645b2364203e958a32500201bd
Marchesi Gabriele <marchesigabriele2819@gmail.com>
2021-11-10 13:22:42 +0100

    reverseIntegers

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -1,19 +1,34 @@
 def main():
-    numero = 123
-    print(reverse(numero))
+    n1 = 125376153716357      
+    n2 = 2147483648
+    print(reverse(n2))
 
 
-def reverse(numero):
-    newNumero = []
-    c = str(numero)
 
-    for k in range(len(c)):
-        if k == "-":
-            newNumero[0] = "-"
-        else:
-            newNumero[len(c) - k - 1] = c[k]
+def reverse(x):
+    c = 0
+    index = 0
+    # flag indicates whether or not the input number is negative. This is used for syntax problem reasons with negative numbers. 
+    flag = False
 
-    return "".join(newNumero)
+    if x < 0:
+        flag = True
+        x = 0 - x
+
+    for k in map(int, str(x)): 
+        print(k)
+        c = c + k * pow(10,index) 
+        print(c)
+        index += 1
+
+    if c > pow(2,31)-1 or c < -pow(2,31):
+        return 0
+
+    if not flag:
+        return c
+    else:
+        return 0-c
+
 
 
 if __name__ == "__main__":
